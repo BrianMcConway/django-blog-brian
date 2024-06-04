27880c683f90c30d24d0bfc246b499587ff3d7d2
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-06-04 16:12:52 +0000

    Add Procfile, install gunicorn, commit & push for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-luib1brh&inbvca1^kc#duimj3sr6-ma+q=d!e5qd((i%7i&q='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-brianmcconw-djangoblogb-x94db0qlo4g.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-brianmcconw-djangoblogb-x94db0qlo4g.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
