06492744ae23b3856101d051f000b347499eab9b
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-06-08 09:55:19 +0000

    Add STATIC_ROOT staticfiles, run python3 manage.py collectstatic, check python version, add runtime.txt & add pythpn version (python3 -V) & add to runtime.txt. Set debug to False.

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = ("524DD9DDCB83F99327FE928FE741D")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-brianmcconw-djangoblogb-x94db0qlo4g.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
